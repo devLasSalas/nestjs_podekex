f86fdf23e8e7f98487520e8105133cd6f31bd362
devlassalas <delassalasospino2003@gmail.com>
2023-10-03 20:44:21 -0500

    v1

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { join } from 'path';
+
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
+    PokemonModule,
+
+    CommonModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
